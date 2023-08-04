4a34847523dc4378d1dec4ab69baeac0a34e81ae
llbox <shuaifei1005@gmail.com>
2023-08-04 22:14:18 +0800

    "change del"

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ func main() {
 	s1 := []int{1, 2, 3, 4, 5}
 	fmt.Println("s1 len:", len(s1))
 	fmt.Println("s1 len:", cap(s1))
-	s2 := week1.DeleteAt[int](s1, 2)
+	s2, err := week1.DeleteAt[int](s1, 4)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println("s2 len:", len(s2))
 	fmt.Println("s2 len:", cap(s2))
 	fmt.Println(s2)
--- week1/SliceDelete.go
+++ week1/SliceDelete.go
@@ -1,9 +1,15 @@
 package week1
 
+import "errors"
+
 // DeleteAt 切片删除指定下标元素
-func DeleteAt[T any](sourceSlice []T, index int) []T {
+func DeleteAt[T any](sourceSlice []T, index int) ([]T, error) {
 	targetSlice := make([]T, 0, len(sourceSlice)-1)
+	if index < 0 || index > len(sourceSlice)-1 {
+		return targetSlice, errors.New("invalid index error")
+	}
+
 	targetSlice = append(targetSlice, sourceSlice[:index]...)
 	targetSlice = append(targetSlice, sourceSlice[index+1:]...)
-	return targetSlice
+	return targetSlice, nil
 }
